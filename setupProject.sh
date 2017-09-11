9933b9bca56d054ccda8370f86410c7b328612a5
Katelyn Kukar <kbkukar@usfca.edu>
2017-09-10 19:40:50 -0700

    add primary bash script to set up directories

---
+++ setupProject.sh
@@ -0,0 +1,38 @@
+# This is a project setup for BioInformatics Homework 1
+# Katelyn Kukar kbkuk@usfca.edu
+# September 11th, 2017
+
+#!/bin/bash
+
+mkdir code data
+touch code/.gitkeep
+mkdir data/raw_data
+cd data
+touch data/.gitkeep
+cd raw_data 
+curl -L http://npk.io/PU1BaY+ -o fasta-archive.zip
+unzip fasta-archive.zip
+
+echo "downloading zip files"
+echo "unzipping zip files"
+
+touch raw_data/.gitkeep
+cd ../..
+
+echo "return to directory main"
+
+mkdir output
+cd output
+touch output/.gitkeep
+mkdir figures
+cd figures
+touch figures/.gitkeep
+cd ..
+mkdir tables
+cd tables
+touch tables/.gitkeep
+
+
+echo "running directories"
+echo "all done"
+
