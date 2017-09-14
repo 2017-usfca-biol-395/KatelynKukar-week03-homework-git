e15291b7955652a4528a3ba8e606deccce8624ba
Katelyn Kukar <kbkukar@usfca.edu>
2017-09-14 08:54:29 -0700

    Commiting updated sequence file based on GitHub comments

--- code/countsequences.sh
+++ code/countsequences.sh
@@ -1,19 +1,17 @@
-# This is a secondary bash script to count sequences in the fasta files
-# Katelyn Kukar, kbkukar@usfca.edu
-#September 11th, 2017
-
-# Using a for loop
 #!/bin/bash
 
-for file in $@
-do
-	wc $file
-done
+# This is a secondary bash script to count sequences in the fasta files
+# Katelyn Kukar, kbkukar@usfca.edu
+# September 11th, 2017
+
+# Using a for loop
+# Set up for loop parameters
 
 for file in $@
 do
-  grep -c "^>" $file
+	echo "This file $file has $(grep -c ">" $file) number or sequences"
 done
 
 
-echo "hopefully it works"
+echo "Number of sequences has been counted and run"
+echo "For Loop completed"
